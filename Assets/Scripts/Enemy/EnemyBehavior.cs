f06e9fd8ed13acc1b4896d0d5ce89069d8e9d782
kirakirahoshi <37220232203780@stu.xmu.edu.cn>
2024-10-06 10:45:40 +0800

    Enemy NPC and Timer

---
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    [SerializeField] private float currentSpeed = 0;
+    public Vector2 movementInput { get; set; }
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        Move();
+        SetAnimation();
+    }
+    private void Move()
+    {
+        if(movementInput.magnitude>0.1f&&currentSpeed>=0)
+        {
+            rb.velocity=movementInput*currentSpeed;
+            if(movementInput.x<0)
+            {
+                sr.flipX = false;
+            }
+            if(movementInput.x > 0)
+            {
+                sr.flipX = true;
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+    private void SetAnimation()
+    {
+        //anim.SetBool("isWalk", movementInput.magnitude > 0);
+    }
+}
---
+++ Assets/Scripts/Enemy/enemyControl.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Pathfinding;
+using System.Threading;
+
+public class enemyControl : MonoBehaviour
+{
+    public UnityEvent<Vector2> OnMoveMentInput;
+    public UnityEvent Ontrigger;
+    [SerializeField] private Transform player;
+    [SerializeField] private float chaseDistance = 3.0f;
+    [SerializeField] private float triggerDistance = 0.5f;
+    private Seeker seeker;
+    private List<Vector3> pathPointList;
+    private int currentIndex = 0;
+    private float pathGenerateInteval = 0.5f;
+    private float pathGenerateTimer = 0f;
+    private bool faceleft = true;
+    private void Awake()
+    {
+        seeker = GetComponent<Seeker>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+            return;
+        float distance = Vector2.Distance(player.position, transform.position);
+        if (distance < chaseDistance && ((faceleft&&player.position.x<=transform.position.x)||(!faceleft&&player.position.x>=transform.position.x)))
+        {
+            AutoPath();
+            if (pathPointList == null)
+                return;
+            //ÔÚ´¥·¢·¶Î§Àï×öÊ²Ã´£¿
+            if (distance < triggerDistance)
+            {
+                OnMoveMentInput?.Invoke(Vector2.zero);
+                Ontrigger?.Invoke();
+            }
+            else
+            {
+                //Vector2 direction = player.position - transform.position;
+                Vector2 direction = (pathPointList[currentIndex]-transform.position).normalized;
+                OnMoveMentInput?.Invoke(direction.normalized);
+            }
+        }
+        else
+        {
+            OnMoveMentInput?.Invoke(Vector2.zero);//Í£Ö¹×·»÷
+        }
+    }
+    private void AutoPath()
+    {
+        pathGenerateTimer += Time.deltaTime;
+        if(pathGenerateTimer>=pathGenerateInteval)
+        {
+            GeneratePath(player.position);
+            pathGenerateTimer = 0;
+        }
+        if(pathPointList == null||pathPointList.Count<=0)
+        {
+            GeneratePath(player.position);
+        }
+        else if (Vector2.Distance(transform.position, pathPointList[currentIndex])<=0.1f)
+        {
+            currentIndex++;
+            if (currentIndex >= pathPointList.Count)
+                GeneratePath(player.position);
+        }
+    }
+    private void GeneratePath(Vector3 target)
+    {
+        currentIndex = 0;
+        seeker.StartPath(transform.position, target, Path =>
+        {
+            pathPointList = Path.vectorPath;
+        });
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color= Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseDistance);
+    }
+}
